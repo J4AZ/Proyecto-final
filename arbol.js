49eee24b48d00df55a6adc35eb1801e080e59ba6
J4AZ <jorgealfaro394@gmail.com>
2025-11-28 11:40:13 -0600

    Arbol

---
+++ arbol.js
@@ -0,0 +1,33 @@
+class Arbol {
+    constructor() {
+        this.raiz = null;
+    }
+
+    preOrder() {
+        const resultado = [];
+        this._preOrder(this.raiz, resultado);
+        return resultado.join(" ");
+    }
+
+    _preOrder(nodo, resultado) {
+        if (nodo) {
+            resultado.push(nodo.valor);
+            this._preOrder(nodo.izq, resultado);
+            this._preOrder(nodo.der, resultado);
+        }
+    }
+
+    postOrder() {
+        const resultado = [];
+        this._postOrder(this.raiz, resultado);
+        return resultado.join(" ");
+    }
+
+    _postOrder(nodo, resultado) {
+        if (nodo) {
+            this._postOrder(nodo.izq, resultado);
+            this._postOrder(nodo.der, resultado);
+            resultado.push(nodo.valor);
+        }
+    }
+}
